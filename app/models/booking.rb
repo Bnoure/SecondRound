5dd3e442aa7aa1beaaf85bf78dd434537d974572
emma puget <puget.emma@gmail.com>
2024-03-05 15:22:37 +0100

    validation
    Signed-off-by: emma puget <puget.emma@gmail.com>

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :game
   belongs_to :user
   enum :status, { new: 1, pending: 2, done: 3, rejected: 9 }
+  Validates :date, presence: true
+  Validates :status, presence: true
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -2,4 +2,7 @@ class Store < ApplicationRecord
   belongs_to :user
   has_many :games
   has_one_attached :picture
+  Validates :name, presence: true
+  Validates :city, presence: true
+  Validates :address, presence: true
 end
