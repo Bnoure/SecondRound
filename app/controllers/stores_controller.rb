7f4aad8a9a7cbc8d5c6dc4a86898de9ae825f01c
Xavier Le Faou <lefaoux@hotmail.fr>
2024-03-05 10:37:30 +0100

    creation des methodes controllers games bookings et stores
    Signed-off-by: Xavier Le Faou <lefaoux@hotmail.fr>

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,11 +1,14 @@
 class StoresController < ApplicationController
   def index
+    @stores = Store.all
   end
 
   def new
+    @store = Store.new
   end
 
   def show
+    @store = Store.find(params[:id])
   end
 
   def create
