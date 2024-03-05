075ea03bac28d2d5a3cda3c18c874e8fb80f0181
Benkerroum Noureddine <noureddine.benkerroum@gmail.com>
2024-03-05 11:57:40 +0100

    correction device
    Signed-off-by: Benkerroum Noureddine <noureddine.benkerroum@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,4 @@
 Rails.application.routes.draw do
-  get 'games/index'
-  get 'games/new'
-  get 'games/show'
-  get 'games/create'
-  get 'games/edit'
-  get 'games/update'
-  get 'games/destroy'
-  get 'bookings/index'
-  get 'bookings/new'
-  get 'bookings/show'
-  get 'bookings/create'
-  get 'stores/index'
-  get 'stores/new'
-  get 'stores/show'
-  get 'stores/create'
-  get 'battles/index'
-  get 'battles/new'
-  get 'battles/show'
-  get 'battles/create'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -25,6 +6,17 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  get 'pages/nour'
+  get 'pages/emma'
+  get 'pages/xavier'
+  get 'pages/dashboard'
+  resources :battles, only: [:index, :show, :new, :create]
+  resources :games
+
+  resources :stores, only: [:index] do
+    resources :bookings
+  end
 
   # Defines the root path route ("/")
   # root "posts#index"
