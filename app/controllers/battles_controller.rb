d4ab68ba3bb04eb38424a023086f1b77f986c395
emma puget <puget.emma@gmail.com>
2024-03-05 12:23:05 +0100

    controller battles
    Signed-off-by: emma puget <puget.emma@gmail.com>

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -1,13 +1,23 @@
 class BattlesController < ApplicationController
   def index
-  end
-
-  def new
+    @battles = Battle.where(user_id: current_user.id)
   end
 
   def show
+    @battle = Battle.find(params[:id])
+  end
+
+  def new
+    @battle = Battle.new
   end
 
   def create
+    @battle = Battle.new(battle_params)
+  end
+
+  private
+
+  def battle_params
+    params.require(:battle).permit(:console, :winning_game_id, :user_id)
   end
 end
