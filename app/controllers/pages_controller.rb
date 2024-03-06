a95560d2147ab90da4da66d5fc7ec026bbcbad25
Xavier Le Faou <lefaoux@hotmail.fr>
2024-03-06 09:29:46 +0100

    finition des cartes store
    Signed-off-by: Xavier Le Faou <lefaoux@hotmail.fr>

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :xavier]
 
   def home
+  end
+
+  def xavier
   end
 end
