c2e40a70a9272abb06006facfbdd621d2bcde12e
emma puget <puget.emma@gmail.com>
2024-03-06 14:50:48 +0100

    query de la loc
    Signed-off-by: emma puget <puget.emma@gmail.com>

--- app/models/store.rb
+++ app/models/store.rb
@@ -5,4 +5,10 @@ class Store < ApplicationRecord
   validates :name, presence: true
   validates :city, presence: true
   validates :address, presence: true
+
+  geocoded_by :city
+  after_validation :geocode, if: :will_save_change_to_address?
+
+  scope :filter_by_game_title, ->(query) { joins(:games).where("title ILIKE ?", "%#{query}}%") }
+  scope :filter_by_localisation, ->(localisation) { near(localisation) }
 end
