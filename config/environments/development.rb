597a8345b5e40992644c1701032ae9c8615be46c
Romain Jumeau <romain.jumeau@hotmail.fr>
2024-03-07 12:02:34 +0100

    config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+
+  config.hosts << "90dd-91-170-171-177.ngrok-free.app"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
