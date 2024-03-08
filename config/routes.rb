68b7bdc1eb7607eb0f201b46a0b5e5f40d679f46
Xavier Le Faou <lefaoux@hotmail.fr>
2024-03-08 11:41:29 +0100

    cards booking routes et method create
    Signed-off-by: Xavier Le Faou <lefaoux@hotmail.fr>

--- config/routes.rb
+++ config/routes.rb
@@ -12,12 +12,11 @@ Rails.application.routes.draw do
   get 'pages/xavier'
   get 'pages/dashboard'
   resources :battles, only: [:index, :show, :new, :create]
-  resources :games
-
-  resources :stores, only: [:index, :show] do
-    resources :bookings
+  resources :games do
+    resources :bookings, only: [:create]
   end
+  resources :bookings, only: [:index, :show]
+  resources :stores, only: [:index, :show]
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+
 end
