f855605f00110c907bdb163e76cb0598e839e927
Benkerroum Noureddine <noureddine.benkerroum@gmail.com>
2024-03-11 17:35:59 +0100

    Ajouter importation de "battle-console" dans _index.scss, autoriser l'accès aux actions create et edit sans authentification dans BattlesController, ajouter la logique de création et d'édition dans les actions create et edit, ajouter une relation belongs_to :console dans Battle, ajouter une relation has_many :battles dans Console, ajouter un formulaire pour sélectionner la console dans new.html.erb, mettre à jour le nom de la console "master system" en "Master system" dans seeds.rb

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -1,5 +1,5 @@
 class BattlesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:new]
+  skip_before_action :authenticate_user!, only: [:new, :create, :edit]
 
   def index
     @battles = Battle.where(user_id: current_user.id)
@@ -13,13 +13,28 @@ class BattlesController < ApplicationController
     @battle = Battle.new
   end
 
+
   def create
-    @battle = Battle.new(battle_params)
+     @battle = Battle.new(battle_params)
+     @battle.user = current_user
+
+    if @battle.save
+      redirect_to edit_battle_path(@battle)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @hide_nav = true
+    @battle = Battle.find(params[:id])
+    @first_games = Game.where(console_id: @battle.console_id).sample(5)
+    @second_games = Game.where(console_id: @battle.console_id).where.not(id: @first_games).sample(5)
   end
 
   private
 
   def battle_params
-    params.require(:battle).permit(:console, :winning_game_id, :user_id)
+    params.require(:battle).permit(:console_id, :winning_game_id)
   end
 end
