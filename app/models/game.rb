c3205b3745a722f2124adf875860a64deecbe9bc
Xavier Le Faou <lefaoux@hotmail.fr>
2024-03-13 15:36:31 +0100

    creation jeu openai
    Signed-off-by: Xavier Le Faou <lefaoux@hotmail.fr>

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,12 +1,13 @@
 class Game < ApplicationRecord
   belongs_to :store
-  belongs_to :console
+  belongs_to :console, optional: true
   has_many :bookings, dependent: :destroy
   has_many :battles, class_name: "battle", foreign_key: "winning_game_id"
   has_one_attached :picture
   validates :store, presence: true
   attr_accessor :game_user_photo
 
+  CONDITIONS = ["Neuf", "Parfait Etat", "Bon Etat", "Etat Correct"]
   scope :filter_by_title, ->(query) { where("title ILIKE ?", "%#{query}%") }
 
   def self.build_with_api_data(api_data)
@@ -15,7 +16,7 @@ class Game < ApplicationRecord
       year: api_data[:year].to_i,
       description: api_data[:description],
       category: api_data[:category],
-      console: Console.find_or_create_by(name: api_data[:console]),
+      english_title: api_data[:english_title]
     )
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_08_150400) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_13_094527) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,7 +51,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_08_150400) do
     t.index ["user_id"], name: "index_battles_on_user_id"
     t.index ["winning_game_id"], name: "index_battles_on_winning_game_id"
   end
-  
+
   create_table "bookings", force: :cascade do |t|
     t.bigint "game_id", null: false
     t.bigint "user_id", null: false
@@ -80,6 +80,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_08_150400) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "console_id"
+    t.string "english_title"
     t.index ["console_id"], name: "index_games_on_console_id"
     t.index ["store_id"], name: "index_games_on_store_id"
   end
