1c78fc0899217d5c7cb1ba36bbdb1ac974731c62
Benkerroum Noureddine <noureddine.benkerroum@gmail.com>
2024-03-14 15:20:38 +0100

    retouche
    Signed-off-by: Benkerroum Noureddine <noureddine.benkerroum@gmail.com>

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ApplicationRecord
   belongs_to :store
   belongs_to :console, optional: true
   has_many :bookings, dependent: :destroy
-  has_many :battles, class_name: "Battle", foreign_key: "winning_game_id"
+  has_many :battles, class_name: "Battle", foreign_key: "winning_game_id", dependent: :destroy
   has_one_attached :picture
   validates :store, presence: true
   attr_accessor :game_user_photo, :game_uuid
